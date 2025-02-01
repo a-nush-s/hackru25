cba019fa879eb4b6de7bae836b7039969353d75b
grayayz <janinealaddin@gmail.com>
2025-02-01 17:18:51 -0500

    add new task

---
+++ option_button.gd
@@ -0,0 +1,27 @@
+extends OptionButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_item_selected(index: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_option_button_2_item_selected(index: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_option_button_3_item_selected(index: int) -> void:
+	pass # Replace with function body.
