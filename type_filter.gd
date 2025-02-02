8d9ed3cea11d1c7c29c94de956373e7378774627
iolive12 <playfieldhockey@aol.com>
2025-02-01 20:10:34 -0500

    Adding options for type filters

--- type_filter.gd
+++ type_filter.gd
@@ -3,11 +3,16 @@ extends PopupMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_check_item("Quiz", 0)
-	add_check_item("Assignment", 1)
+	add_check_item("Assignment", 0)
+	add_check_item("Discussion Post", 1)
 	add_check_item("Paper", 2)
-	add_check_item("Exam", 3)
+	add_check_item("Project", 3)
+	add_check_item("Quiz", 4)
+	add_check_item("Midterm/Exam", 5)
+	add_check_item("Project", 6)
+
 	index_pressed.connect(_index_pressed)
+	# assignment, discussion, paper, project, quiz, midterm/exam, final
 	
 	
 
@@ -15,32 +20,46 @@ func _ready():
 func _index_pressed(index: int):
 	# need to uncheck all other categories (Type filter, Due date filter)
 	if index == 0:
-		print("Quiz was pressed")
 		if (is_item_checked(0)):
 			set_item_checked(0, false)
 		else: 
 			set_item_checked(0, true)
 	
 	if index == 1:
-		print("Assignment was pressed")
 		if (is_item_checked(1)):
 			set_item_checked(1, false)
 		else: 
 			set_item_checked(1, true)
 			
 	if index == 2:
-		print("Paper was pressed")
 		if (is_item_checked(2)):
 			set_item_checked(2, false)
 		else: 
 			set_item_checked(2, true)
 		
 	if index == 3:
-		print("Exam was pressed")
 		if (is_item_checked(3)):
 			set_item_checked(3, false)
 		else: 
 			set_item_checked(3, true)
+			
+	if index == 4:
+		if (is_item_checked(4)):
+			set_item_checked(4, false)
+		else: 
+			set_item_checked(4, true)
+			
+	if index == 5:
+		if (is_item_checked(5)):
+			set_item_checked(5, false)
+		else: 
+			set_item_checked(5, true)
+			
+	if index == 6:
+		if (is_item_checked(6)):
+			set_item_checked(6, false)
+		else: 
+			set_item_checked(6, true)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
