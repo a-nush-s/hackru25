c84e744796e95853e2849c5a9040f9d8e44d6be9
grayayz <janinealaddin@gmail.com>
2025-02-02 10:41:49 -0500

    ....

--- Icon_Class.gd
+++ Icon_Class.gd
@@ -58,6 +58,28 @@ func set_theme(theme : int):
 		small == smallLight
 		large == largeLight
 
+#func _ready():
+#	add_radio_check_item("Dark Mode", 0)
+#	add_radio_check_item("Light Mode", 1)
+#	index_pressed.connect(_index_pressed)
+#	"res://class.gd"
+	
+
+
+#func _index_pressed(index: int):
+	# need to uncheck all other categories (Type filter, Due date filter)
+#	if index == 0:
+#		set_item_checked(0, true)
+#		set_item_checked(1, false)
+#		get_tree().change_scene_to_file("res://buttonhomepage.tscn")
+#		#change theme of icon to dark
+
+	
+#	if index == 1:
+#		set_item_checked(1, true)
+#		set_item_checked(0, false)
+#		get_tree().change_scene_to_file("res://buttonhomepagelight.tscn")
+		#change theme of icon to light
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
