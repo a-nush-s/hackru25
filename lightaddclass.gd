1b98b21c260039233cc9281fcd93716fa6f25bd6
a-nush-s <anusikdar@gmail.com>
2025-02-02 06:54:31 -0500

    idk

--- lightaddclass.gd
+++ lightaddclass.gd
@@ -3,8 +3,6 @@ extends Control
 @onready var color_picker : ColorPickerButton = $ColorPickerButton
 @onready var name_input : LineEdit = $NameInput
 
-func _on_addclassbtn_pressed() -> void:
-	self.show()
 	
 func _on_exit_pressed() -> void:
 	self.hide()
@@ -15,3 +13,7 @@ func _on_add_task_button_pressed() -> void:
 	new_class.set_class(self.name_input.text, self.color_picker.color)
 	
 	Global.add_class(new_class)
+
+
+func _on_addclassbtn_2_pressed() -> void:
+	self.show() # Replace with function body.
